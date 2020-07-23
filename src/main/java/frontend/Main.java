56052e874a37333da63dd4c5c32264244448d14d
Bartosz Ujazdowski <b.ujazdowski@gmail.com>
2020-07-23 10:44:13 +0200

    Pices

--- src/main/java/frontend/Main.java
+++ src/main/java/frontend/Main.java
@@ -6,6 +6,7 @@ import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Box;
 
 public class Main extends SimpleApplication {
@@ -17,9 +18,14 @@ public class Main extends SimpleApplication {
 
     private void createChessBoard() {
         Geometry chessBoard = (Geometry) assetManager.loadModel("assets/Models/Chessboard/12951_Stone_Chess_Board_v1_L3.obj");
-        chessBoard.setLocalTranslation(chessBoard.getModelBound().getCenter().mult(-1));
         chessBoard.rotate((float) (-Math.PI / 2.0), 0, 0);
         rootNode.attachChild(chessBoard);
+    }
+
+    private void loadPawn() {
+        Geometry pawn = (Geometry) assetManager.loadModel("assets/Models/Pawn/Pawn.obj");
+        pawn.scale(100f);
+        rootNode.attachChild(pawn);
     }
 
     @Override
@@ -30,13 +36,14 @@ public class Main extends SimpleApplication {
         al.setColor(ColorRGBA.White.mult(1.2f));
         rootNode.addLight(al);
 
-        Box b = new Box(1, 1, 1); // create cube shape
+        Box b = new Box(0.5f, 0.5f, 0.5f);
         Geometry geom = new Geometry("Box", b);
-        Material mat = new Material(assetManager,
-                "Common/MatDefs/Misc/Unshaded.j3md");  // create a simple material
-        mat.setColor("Color", ColorRGBA.Blue);   // set color of material to blue
+        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        mat.setColor("Color", ColorRGBA.Blue);
         geom.setMaterial(mat);
         rootNode.attachChild(geom);
+
+        loadPawn();
 
         createChessBoard();
     }
